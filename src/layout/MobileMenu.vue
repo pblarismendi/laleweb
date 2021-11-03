b475b157fceab26aa4d1e23d2d0aa4c9404a3534
Pablo Arismendi <pblarismendi@gmail.com>
2021-11-03 20:56:24 -0300

    add theme vue material kit

---
+++ src/layout/MobileMenu.vue
@@ -0,0 +1,27 @@
+<template>
+<!--  ><md-list-item-->
+<!--    href="https://www.instagram.com/CreativeTimOfficial"-->
+<!--    target="_blank"-->
+<!--  >-->
+<!--    <i class="fab fa-instagram"></i>-->
+<!--    <p class="hidden-lg">Instagram</p>-->
+<!--    <md-tooltip md-direction="bottom">Follow us on Instagram</md-tooltip>-->
+<!--  </md-list-item>-->
+  <md-list
+    class="nav-mobile-menu nav-mobile-section-start"
+    v-if="navMobileSectionStart === 'true'"
+  >
+  </md-list>
+</template>
+
+<script>
+export default {
+  name: "mobile-menu",
+  props: {
+    navMobileSectionStart: {
+      type: String,
+      default: "false"
+    }
+  }
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  css: {
+    loaderOptions: {
+      css: {
+        sourceMap: process.env.NODE_ENV !== "production" ? true : false
+      }
+    }
+  }
+};
